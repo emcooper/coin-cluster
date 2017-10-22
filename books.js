0186d2896aa81bbc333a0bc2daf34f4bffd05ed9
Ellen Cooper <ellencooper74@gmail.com>
2017-10-22 10:02:22 -0600

    removes redundant variables and console.logs

--- books.js
+++ books.js
@@ -1,36 +1,27 @@
 module.exports = {makeCall}
-var pry = require('pryjs');
 
 var request = require('request');
-let exchanges = [{name: "Bittrex",
-                  url: "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-ETH&type=both"},
-                 {name: "Poloniex",
-                 url: "https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH"}];
 
- let url1 = "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-ETH&type=both"
- let url2 = "https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH"
+let url1 = "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-ETH&type=both"
+let url2 = "https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH"
 
-let results = [1]
-let bittrexResults = []
-let poloniexResults = []
+var data = ""
+
 
 function requestAsync(url) {
     return new Promise(function(resolve, reject) {
         request(url, function(err, res, body) {
             if (err) { return reject(err); }
-            json = JSON.parse(body)
-            return resolve(json);
+            return resolve(JSON.parse(body));
         });
 
     });
 }
-var data = ""
 
 function makeCall(){
   Promise.all([requestAsync(url1), requestAsync(url2)])
       .then(function(allData) {
-        console.log(allData[1])
-          data = allData[1]["asks"] + "hi"
+          data = allData[1]["asks"].toString()
       });
   return data
 }
