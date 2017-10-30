5cf277619d7e2446feb987d2176b3e3fc57c4123
Ellen Cooper <ellencooper74@gmail.com>
2017-10-30 16:21:34 -0600

    semi-colon removal

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const express = require('express')
+const SocketServer = require('ws').Server
+const path = require('path')
 const books = require('./lib/books')
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const PORT = process.env.PORT || 3000
+const INDEX = path.join(__dirname, 'index.html')
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({ server })
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+  console.log('Client connected')
+  ws.on('close', () => console.log('Client disconnected'))
+})
 
 setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send(books.getOrders());
-  });
-}, 500);
+    client.send(books.getOrders())
+  })
+}, 500)
