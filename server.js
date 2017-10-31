9fab70615ca253e0b2a595fee4edbbd6571d4c2b
Ellen Cooper <ellencooper74@gmail.com>
2017-10-31 15:54:03 -0600

    replaces ws with socket

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express')
-const SocketServer = require('ws').Server
-const path = require('path')
+const http = require("http");
+// const SocketServer = require('ws').Server
+// const path = require('path')
 const books = require('./lib/books')
-const PORT = process.env.PORT || 3000
-const INDEX = path.join(__dirname, 'index.html')
+const port = process.env.PORT || 3000
+// const INDEX = path.join(__dirname, 'index.html')
+const socketIo = require("socket.io");
+const index = require("./routes/index");
+const app = express();
+app.use(index)
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Listening on port ${port}`))
 
-const wss = new SocketServer({ server })
+const io = socketIo(server)
+function getApiAndEmit(socket){
+  socket.emit("FromAPI", books.getOrders())
+  console.log(books.getOrders())
+}
 
-wss.on('connection', (ws) => {
-  console.log('Client connected')
-  ws.on('close', () => console.log('Client disconnected'))
-})
+// const wss = new SocketServer({ server })
+//
+// wss.on('connection', (ws) => {
+//   console.log('Client connected')
+//   ws.on('close', () => console.log('Client disconnected'))
+// })
+//
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(books.getOrders())
+//   })
+// }, 500)
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(books.getOrders())
-  })
-}, 500)
+let interval;
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
