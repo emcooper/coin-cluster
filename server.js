e7541bc2008db5d4513bb2f73e7062a410a9d90a
Ellen Cooper <ellencooper74@gmail.com>
2017-11-02 17:08:00 -0600

    removes semi-colons

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const http = require("http");
+const http = require("http")
 const books = require('./lib/books')
 const port = process.env.PORT || 8080
-const socketIo = require("socket.io");
-const index = require("./routes/index");
-const app = express();
+const socketIo = require("socket.io")
+const index = require("./routes/index")
+const app = express()
 app.use(index)
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
 const io = socketIo(server)
@@ -15,14 +15,14 @@ function getApiAndEmit(socket){
   socket.emit("orders", books.getOrders())
 }
 
-let interval;
+let interval
 io.on("connection", socket => {
-  console.log("New client connected");
+  console.log("New client connected")
   if (interval) {
-    clearInterval(interval);
+    clearInterval(interval)
   }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  interval = setInterval(() => getApiAndEmit(socket), 1000)
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
+    console.log("Client disconnected")
+  })
+})
